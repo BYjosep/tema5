4b1997ee0ce822a491d4462c589666e6f76d902d
Josep Vicent Mestre Llobell <jmestrellobell@gmail.com>
2024-11-06 17:10:48 +0100

    ejercicio2 XD

--- src/main/java/com/BYjosep04/tema05/Ejercicio2.java
+++ src/main/java/com/BYjosep04/tema05/Ejercicio2.java
@@ -1,34 +1,39 @@
 package com.BYjosep04.tema05;
 
-// Escribe un programa que muestre el numero
-// de vocales y el numero de consonantes de una frase.
-
 import java.util.Scanner;
 
 public class Ejercicio2 {
 
     public static void main(String[] args) {
-    String frase;
-    Scanner scanner = new Scanner(System.in);
+        String frase;
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Ingrese la frase");
         frase = scanner.nextLine();
         scanner.close();
+        System.out.println(resultado(frase));
     }
 
 
-    public static int contador(String frase){
-        int longitud, resultado;
-        longitud= frase.length();
+    public static String resultado(String frase) {
+        int longitud;
+        String resultado;
+        int vocales, consonantes;
+        vocales = 0;
+        consonantes = 0;
+        longitud = frase.length();
 
 
-        char valorPosicion = String.valueOf(frase.charAt(i));
-        for (int i = 0; i < longitud-1; i++) {
-            switch (){
-                case 'a'-> System.out.println("hola");
+        //char[] test = frase.toCharArray();
+        for (int i = 0; i < longitud; i++) {
+
+            switch (frase.charAt(i)) {
+                case 'a', 'e', 'i', 'o', 'u' -> vocales = +1;
+                case 'q', 'w', 'r', 't', 'y', 'p', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l',
+                     'z', 'x', 'c', 'v', 'b', 'n', 'm' -> consonantes += 1;
             }
 
         }
-
-    return resultado;
+        resultado = "El resultado ha sido de "+ vocales+" vocales y "+consonantes+" consonates";
+        return resultado;
     }
 }
