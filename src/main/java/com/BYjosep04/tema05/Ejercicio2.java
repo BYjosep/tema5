74aaf9e0eb4cdf9c2adf4b2c188a46068f2c90d6
Josep Vicent Mestre Llobell <jmestrellobell@gmail.com>
2024-11-07 00:46:37 +0100

    Ejercicio3 Terminado

--- src/main/java/com/BYjosep04/tema05/Ejercicio2.java
+++ src/main/java/com/BYjosep04/tema05/Ejercicio2.java
@@ -13,27 +13,37 @@ public class Ejercicio2 {
         System.out.println(resultado(frase));
     }
 
-
+    /**
+     * @param frase El parametro es un String
+     * @return devuelve una frase con el numero de vocales y consonantes
+     */
     public static String resultado(String frase) {
+        // variables
         int longitud;
         String resultado;
         int vocales, consonantes;
         vocales = 0;
         consonantes = 0;
+        /*
+         * Para evitar tener que poner mas letras en el swich
+         * se han pasado todos los caracteres a minusculas
+         */
         frase = frase.toLowerCase();
         longitud = frase.length();
 
-        //char[] test = frase.toCharArray();
         for (int i = 0; i < longitud; i++) {
 
             switch (frase.charAt(i)) {
                 case 'a', 'e', 'i', 'o', 'u' -> vocales += 1;
-                case 'q', 'w', 'r', 't', 'y', 'p', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l',
-                     'z', 'x', 'c', 'v', 'b', 'n', 'm' -> consonantes += 1;
+                case 'q', 'w', 'r', 't',
+                     'y', 'p', 's', 'd',
+                     'f', 'g', 'h', 'j',
+                     'k', 'l', 'z', 'x',
+                     'c', 'v', 'b', 'n', 'm' -> consonantes += 1;
             }
-
         }
-        resultado = "El resultado ha sido de "+ vocales+" vocales y "+consonantes+" consonates";
+        resultado = "El resultado ha sido de " + vocales + " vocales y " + consonantes + " consonates";
         return resultado;
+
     }
 }
---
+++ src/main/java/com/BYjosep04/tema05/Ejercicio3.java
@@ -0,0 +1,39 @@
+package com.BYjosep04.tema05;
+
+import java.util.Scanner;
+
+public class Ejercicio3 {
+
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingresre una frase:");
+        sb.append(scanner.nextLine());
+
+        System.out.printf("La frase tiene %d palabras",contadorPalabras(sb));
+    }
+
+    /**
+     *
+     * @param mensaje Variable como {@link StringBuilder StringBuilder}
+     * @return Devuelve un entero como valor
+     */
+    public static int contadorPalabras(StringBuilder mensaje){
+        String mensajeStr = mensaje.toString();
+
+
+        return  mensajeStr.split("\\s|\n|,").length;
+    }
+
+    /**
+     *
+     * @param mensaje Variable como {@link String String}
+     * @return Devuelve un entero como valor
+     */
+    public static int contadorPalabras(String mensaje){
+
+
+        return  mensaje.split("\\s|\n|,").length;
+    }
+
+}
