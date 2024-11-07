159cb4bafc4b48099dd2ffd97c8caf737e29a636
Josep Vicent Mestre Llobell <jmestrellobell@gmail.com>
2024-11-07 20:17:39 +0100

    Ejercicio7 hay que terminar

---
+++ src/main/java/com/BYjosep04/tema05/Ejercicio7.java
@@ -0,0 +1,12 @@
+package com.BYjosep04.tema05;
+
+import java.util.Scanner;
+
+public class Ejercicio7 {
+    public static void main(String[] args) {
+        String frase;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese una frase");
+        frase = scanner.nextLine();
+    }
+}
---
+++ src/main/java/com/BYjosep04/tema05/TODO.java
@@ -0,0 +1,7 @@
+package com.BYjosep04.tema05;
+
+public class TODO {
+    public static void main(String[] args) {
+        //TODO hacer hasta el 10
+    }
+}
