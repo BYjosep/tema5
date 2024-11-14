50abb36514af00411d75d9a335174211fb125217
Josep Vicent Mestre Llobell <jmestrellobell@gmail.com>
2024-11-14 18:57:16 +0100

    JavaDoc creado en el metodo

--- src/main/java/com/BYjosep04/tema05/Ejercicio15.java
+++ src/main/java/com/BYjosep04/tema05/Ejercicio15.java
@@ -10,6 +10,14 @@ public class Ejercicio15 {
         System.out.println(matrizDeUnCaracter(caracter, columna, fila));
     }
 
+
+    /**
+     *
+     * @param caracter El caracter que se desea usar
+     * @param columna El numero de columnas que se desea usar
+     * @param fila El numero de filas que desea usar
+     * @return Devuelve un {@link String String} con la tabla deseada
+     */
     public static String matrizDeUnCaracter(char caracter, int columna, int fila) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < fila; i++) {
