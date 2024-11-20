f6dd527c90976e5caec2ba52d660cf4192c8877b
Josep Vicent Mestre Llobell <jmestrellobell@gmail.com>
2024-11-20 17:13:07 +0100

    update

--- src/main/java/com/BYjosep04/tema05/Arr/Ejercicio9.java
+++ src/main/java/com/BYjosep04/tema05/Arr/Ejercicio9.java
@@ -6,14 +6,15 @@ public class Ejercicio9 {
     public static void main(String[] args) {
         int[] array= new int[10];
         LibStrings.generadorAleatorio(array, 0,50);
-        int opcion= LibStrings.ingresarUnNumero("MENÚ PRINCIPAL \n"+
-                "==============\n"+
-                "1.-Rellenar array.\n"+
-        "2.-Visualizar contenido del array\n"+
-        "3.-Visualizar contenido par.\n"+
-        "4.-Visualizar contenido múltiplo de 3\n"+
-        "0.-Salir del menú.\n"+
-        "Selecciona una opción:",0,4);
+        int opcion= LibStrings.ingresarUnNumero("""
+                MENÚ PRINCIPAL
+                ==============
+                1.-Rellenar array.
+                2.-Visualizar contenido del array
+                3.-Visualizar contenido par.
+                4.-Visualizar contenido múltiplo de 3
+                0.-Salir del menú.
+                Selecciona una opción:""",0,4);
 
         switch (opcion){
             case 1->{}
