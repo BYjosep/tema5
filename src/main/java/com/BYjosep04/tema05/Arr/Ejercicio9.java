d9df52c93c0fea1b3766ab3911deb5ad8a46846f
Josep Vicent Mestre Llobell <jmestrellobell@gmail.com>
2024-11-21 00:44:32 +0100

    Ejercicio9 terminado

--- src/main/java/com/BYjosep04/tema05/Arr/Ejercicio9.java
+++ src/main/java/com/BYjosep04/tema05/Arr/Ejercicio9.java
@@ -2,11 +2,15 @@ package com.BYjosep04.tema05.Arr;
 
 import com.BYjosep04.tema05.lib.LibStrings;
 
+import java.util.Arrays;
+
 public class Ejercicio9 {
     public static void main(String[] args) {
         int[] array= new int[10];
+        int opcion;
         LibStrings.generadorAleatorio(array, 0,50);
-        int opcion= LibStrings.ingresarUnNumero("""
+        do {
+            opcion = LibStrings.ingresarUnNumero("""
                 MENÚ PRINCIPAL
                 ==============
                 1.-Rellenar array.
@@ -16,14 +20,17 @@ public class Ejercicio9 {
                 0.-Salir del menú.
                 Selecciona una opción:""",0,4);
 
-        switch (opcion){
-            case 1->{}
-            case 2->{}
-            case 3->{}
-            case 4->{}
-            case 0->{}
+            switch (opcion) {
+                case 1 -> crearArray(array);
+                case 2 -> visualizarArray(array);
+                case 3 -> visualizarPares(array);
+                case 4 -> visualizarMultiplos3(array);
+                case 0 -> {
+                }
 
-        }
+            }
+        } while (opcion != 0);
+
     }
 
     public static void crearArray (int[] array) {
@@ -31,7 +38,7 @@ public class Ejercicio9 {
     }
 
     public static void visualizarArray (int[] array) {
-        String arrayStr= array.toString();
+        String arrayStr = Arrays.toString(array);
         System.out.println(arrayStr);
     }
     public static void visualizarPares(int[] array) {
@@ -41,8 +48,32 @@ public class Ejercicio9 {
             if(i %2==0)
                 sb.append(array[i]).append(",");
         }
-        sb.delete(sb.length()-1,sb.length()-1);
-        sb.append("]");
+        String arrayStr = formatearArray(sb);
 
+        System.out.println(arrayStr);
+    }
+
+
+    public static void visualizarMultiplos3(int[] array) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 3 == 0)
+                sb.append("En la posicion ").append(i).append(" esta el numero; ").append(array[i]).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+
+    /**
+     * @param texto texto en formato {@link StringBuilder StringBuilder}
+     *              para eliminar la última coma
+     * @return
+     */
+    public static String formatearArray(StringBuilder texto) {
+        texto.delete(texto.length() - 1, texto.length());
+        texto.append("]");
+        return texto.toString();
     }
 }
--- src/main/java/com/BYjosep04/tema05/TODO.java
+++ src/main/java/com/BYjosep04/tema05/TODO.java
@@ -3,5 +3,9 @@ package com.BYjosep04.tema05;
 public class TODO {
     public static void main(String[] args) {
     //Todo Hasta el 10
+    /*TODO apartir del ejercicio9 hasta el final poner
+        los metodosdirectamente en la libreria buena
+
+     */
     }
 }
