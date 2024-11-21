a8cb18a32adcc2c444ff7105f3343a832dbdb587
Josep Vicent Mestre Llobell <jmestrellobell@gmail.com>
2024-11-21 19:13:16 +0100

    Ejercicio11 terminado
    Signed-off-by: Josep Vicent Mestre Llobell <jmestrellobell@gmail.com>

---
+++ src/main/java/com/BYjosep04/tema05/Arr/Ejercicio11.java
@@ -0,0 +1,31 @@
+package com.BYjosep04.tema05.Arr;
+
+import com.BYjosep04.tema05.lib.LibStrings;
+
+import java.util.Arrays;
+
+public class Ejercicio11 {
+    public static void main(String[] args) {
+        int[] v = new int[10];
+        int[] p;
+        LibStrings.generadorAleatorio(v, Integer.MIN_VALUE, Integer.MAX_VALUE - 1);
+        p = invertirOrdenDeArray(v);
+
+        System.out.println(Arrays.toString(v));
+        System.out.println(Arrays.toString(p));
+    }
+
+    public static int[] invertirOrdenDeArray(int[] array) {
+        if (array == null) {
+            return null;
+        }
+        int cantidad = array.length;
+        cantidad--;
+        int[] retorno = new int[array.length];
+        for (int i : array) {
+            retorno[cantidad] = i;
+            cantidad--;
+        }
+        return retorno;
+    }
+}
--- src/main/java/com/BYjosep04/tema05/TODO.java
+++ src/main/java/com/BYjosep04/tema05/TODO.java
@@ -2,7 +2,9 @@ package com.BYjosep04.tema05;
 
 public class TODO {
     public static void main(String[] args) {
-    //Todo Hasta el 10
+        //Todo Hasta el fin
+
+
     /*TODO apartir del ejercicio9 hasta el final poner
         los metodosdirectamente en la libreria buena
 
