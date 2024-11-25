4d6f5381597a9281f54e3171fd5c569b40434ed6
Josep Vicent Mestre Llobell <jmestrellobell@gmail.com>
2024-11-25 20:18:19 +0100

    update
    Signed-off-by: Josep Vicent Mestre Llobell <jmestrellobell@gmail.com>

--- src/main/java/com/BYjosep04/tema05/TODO.java
+++ src/main/java/com/BYjosep04/tema05/TODO.java
@@ -3,6 +3,7 @@ package com.BYjosep04.tema05;
 public class TODO {
     public static void main(String[] args) {
 
+        //Todo hacer en casa Ejercicio14 arrays
 
     /*TODO apartir del ejercicio9 hasta el final poner
         los metodosdirectamente en la libreria buena
