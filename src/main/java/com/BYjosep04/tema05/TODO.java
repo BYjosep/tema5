de382b457634703100145d91574bda8782c95fe6
Josep Vicent Mestre Llobell <jmestrellobell@gmail.com>
2024-11-25 19:30:16 +0100

    Update Todo, no se si el 13 funciona

--- src/main/java/com/BYjosep04/tema05/TODO.java
+++ src/main/java/com/BYjosep04/tema05/TODO.java
@@ -2,7 +2,6 @@ package com.BYjosep04.tema05;
 
 public class TODO {
     public static void main(String[] args) {
-        //Todo Hasta el fin
 
 
     /*TODO apartir del ejercicio9 hasta el final poner
