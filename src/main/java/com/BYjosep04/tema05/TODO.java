567bb6f72057a0e11da74c17c16dfaf579164367
Josep Vicent Mestre Llobell <jmestrellobell@gmail.com>
2024-12-04 17:09:03 +0100

    update
    Signed-off-by: Josep Vicent Mestre Llobell <jmestrellobell@gmail.com>

--- src/main/java/com/BYjosep04/tema05/TODO.java
+++ src/main/java/com/BYjosep04/tema05/TODO.java
@@ -3,7 +3,7 @@ package com.BYjosep04.tema05;
 public class TODO {
     public static void main(String[] args) {
 
-        //Todo hacer en casa Ejercicio14 arrays
+        //Todo hacer en casa 2
 
 
 
--- src/main/java/com/BYjosep04/tema05/matrices/Ejercicio3.java
+++ src/main/java/com/BYjosep04/tema05/matrices/Ejercicio3.java
@@ -1,8 +1,8 @@
 package com.BYjosep04.tema05.matrices;
 
 import com.BYjosep04.tema05.lib.LibStrings;
-import com.BYjosep04.tema05.matrices.Ejercicio3.Ficha;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Ejercicio3 {
@@ -15,26 +15,26 @@ public class Ejercicio3 {
         PLAYER2
     }
 
-    public static Ficha[][] matriz = new Ficha[3][3];
-    public static Jugador turnoActual = null;
+    private static Ficha[][] tablero;
+    private static Jugador turnoActual;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         boolean seguirJugando;
 
         do {
-            reset(turnoActual);
-
+            reset();
+            play();
 
             System.out.println("¿Desea volver a jugar? S/N");
             char opcion = Character.toLowerCase(scanner.nextLine().charAt(0));
             seguirJugando = opcion == 's';
         } while (seguirJugando);
 
-
+        LibStrings.cerrarScaner();
     }
 
-    public static void play(Ficha[][] matriz) {
+    public static void play() {
         boolean jugadaValida;
         int columna, fila;
         /*
@@ -42,28 +42,55 @@ public class Ejercicio3 {
         se da por hecho que la casilla central está en la cordenada 2,2
          */
         do {
+            System.out.println("Turno " + turnoActual + "(" + getFichaJugada(turnoActual) + ")");
             fila = LibStrings.ingresarUnNumero("Indique en que fila desea poner la ficha", 1, 3);
             fila--;
             columna = LibStrings.ingresarUnNumero("Indique la columna desea poner la ficha", 1, 3);
             columna--;
 
             jugadaValida = esJugadaValida(fila, columna);
+            if (!jugadaValida) {
+                System.err.println("La posicion ya esta ocupada");
+            }
         } while (!jugadaValida);
-
-        matriz[fila][columna] = Ficha;
-
+        tablero[fila][columna] = getFichaJugada(turnoActual);
 
     }
 
-    public static void reset(Jugador turnoActual) {
-        matriz = new Ficha[3][3];
-        turnoActual = null;
+    private static void reset() {
+        Random random = new Random();
+        for (int i = 0; i < tablero.length; i++) {
+            for (int j = 0; j < tablero[i].length; j++) {
+                tablero[i][j] = null;
+            }
+        }
+        Jugador[] valoresJugadores = Jugador.values();
+        turnoActual = valoresJugadores[random.nextInt(0, valoresJugadores.length)];
 
     }
 
-    public static boolean esJugadaValida(int fila, int columna) {
+    private static boolean esJugadaValida(int fila, int columna) {
 
-        return matriz[fila][columna] == null;
+        return tablero[fila][columna] == null;
+    }
+
+    private static String tabletoToString() {
+        StringBuilder sb = new StringBuilder();
+
+
+        for (int i = 0; i < tablero.length; i++) {
+            sb.append("|---|---|---|\n");
+            for (int j = 0; j < tablero[i].length; j++) {
+                sb.append("| ").append(tablero[i][j]).append(" ");
+            }
+            sb.append("|\n");
+        }
+        sb.append("|---|---|---|\n");
+        return sb.toString();
+    }
+
+    private static Ficha getFichaJugada(Jugador jugador) {
+        return Ficha.values()[jugador.ordinal()];
     }
 
 }
